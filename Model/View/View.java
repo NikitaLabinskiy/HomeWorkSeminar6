537630633f4e67fc5c35a3681e336804f9e31e64
Nikita Labinskiy <NLabinskiy@gmail.com>
2023-04-28 20:48:47 +0300

    Add class View

---
+++ Model/View/View.java
@@ -0,0 +1,31 @@
+package Model.View;
+
+import Model.Classes.Product;
+import Model.Classes.WareHouse;
+import Model.Interfaces.Message;
+import Model.Interfaces.WareHouseInfo;
+
+public class View implements WareHouseInfo, Message {
+    @Override
+    public void homeDelivery(Product product, String address) {
+        System.out.println("\nПродукт: " + product.getName() + " отправлен по адресу - " + address);
+    }
+    @Override
+    public void selfDelivery(Product product) {
+        System.out.println("\nПродукт: " + product.getName() + " отгружен на склад");
+    }
+    @Override
+    public void getWareHouseInfo(WareHouse wareHouse) {
+        System.out.println();
+        for (Product product: wareHouse.getStorage()) {
+            System.out.println(product);
+        }
+    }
+    @Override
+    public void getWarehouseAmount(WareHouse wareHouse) {
+        System.out.println("\n" + wareHouse.getWareHouseAmount());
+    }
+    public void printProduct(Product product){
+        System.out.println(product);
+    }
+}
