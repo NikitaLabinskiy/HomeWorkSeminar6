99526821e4484e3dc9f8154d48ac10415a14a7d8
Nikita Labinskiy <NLabinskiy@gmail.com>
2023-04-28 20:45:00 +0300

    Add interface SelfDelivery

---
+++ Model/Interfaces/SelfDeliveryInterface.java
@@ -0,0 +1,8 @@
+package Model.Interfaces;
+
+import Model.Classes.Product;
+import Model.View.View;
+
+public interface SelfDeliveryInterface {
+    public void deliverProduct(Product product, View view);
+}
