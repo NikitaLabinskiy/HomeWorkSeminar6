2f754e8d61f4f175b94d670e4e6e8d5cdffd90f3
Nikita Labinskiy <NLabinskiy@gmail.com>
2023-04-28 21:02:46 +0300

    New commit

--- Model/Interfaces/SelfDeliveryInterface.java
+++ Model/Interfaces/SelfDeliveryInterface.java
@@ -1,7 +1,7 @@
 package Model.Interfaces;
 
 import Model.Classes.Product;
-import Model.View.View;
+import View.View;
 
 public interface SelfDeliveryInterface {
     public void deliverProduct(Product product, View view);
