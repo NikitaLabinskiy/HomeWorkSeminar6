33b49de39b86d50bfc5c85c4106feb14c127eb71
Nikita Labinskiy <NLabinskiy@gmail.com>
2023-04-28 20:44:34 +0300

    Add interface ProductSearcher

---
+++ Model/Interfaces/ProductSearcher.java
@@ -0,0 +1,10 @@
+package Model.Interfaces;
+
+import Model.Classes.WareHouse;
+import Model.View.View;
+
+public interface ProductSearcher {
+    public void findProductByName(WareHouse wareHouse, String productName, View view);
+    public void findProductByPrice(WareHouse wareHouse, Double productPriceMin, Double productPriceMax, View view);
+    public void findProductByAmount(WareHouse wareHouse, Integer amount, View view);
+}
