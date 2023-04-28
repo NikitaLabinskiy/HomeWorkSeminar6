fb39b75d240f26b3d04b3582524b7e8e0a271fb3
Nikita Labinskiy <NLabinskiy@gmail.com>
2023-04-28 21:02:11 +0300

    New commit

--- Model/Interfaces/HomeDeliveryInterface.java
+++ Model/Interfaces/HomeDeliveryInterface.java
@@ -1,7 +1,7 @@
 package Model.Interfaces;
 
 import Model.Classes.Product;
-import Model.View.View;
+import View.View;
 
 public interface HomeDeliveryInterface {
     public void deliverProduct(Product product, String address, View view);
