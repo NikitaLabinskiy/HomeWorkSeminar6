0492f884d5beb14651c94cf7833764a8b224c51b
Nikita Labinskiy <NLabinskiy@gmail.com>
2023-04-28 21:01:49 +0300

    New commit

--- Model/Classes/SelfDelivery.java
+++ Model/Classes/SelfDelivery.java
@@ -1,7 +1,7 @@
 package Model.Classes;
 
 import Model.Interfaces.SelfDeliveryInterface;
-import Model.View.View;
+import View.View;
 
 public class SelfDelivery implements SelfDeliveryInterface {
     @Override
