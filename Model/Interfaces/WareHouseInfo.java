97e3f540ca4cf4f396dc776e439533028b6f0b3b
Nikita Labinskiy <NLabinskiy@gmail.com>
2023-04-28 20:45:40 +0300

    Add interface WarehouseInfo

---
+++ Model/Interfaces/WareHouseInfo.java
@@ -0,0 +1,8 @@
+package Model.Interfaces;
+
+import Model.Classes.WareHouse;
+
+public interface WareHouseInfo {
+    public void getWareHouseInfo(WareHouse wareHouse);
+    public void getWarehouseAmount(WareHouse wareHouse);
+}
